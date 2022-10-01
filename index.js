46355ccb8c819ec64f1628b51148aed1be8d45eb
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-30 20:11:39 -0400

    commit tree fcc7673bdee5b1a849436a9433fef7d13cbfc85f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution here
+function findMatching (drivers, name) {
+    const newDrivers = drivers.filter(person =>  person.toLowerCase() === name.toLowerCase())
+    return newDrivers
+}
+
+findMatching()
